9441aabd0e1d61a56cf386323f73258821362bec
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2018-01-03 22:00:57 +0100

    HLint suggestions

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Data.Matrix
 import qualified Data.Vector as Vector
-import qualified Reader as Reader
+import qualified Reader
 import qualified Data.Maybe as Maybe
 
 main :: IO ()
@@ -18,18 +18,18 @@ main = do
 iterate :: Matrix Int -> Matrix Int
 iterate m = 
     if hasToGrow then
-        matrix ((nrows m)+2) ((ncols m)+2) (\(i,j) -> builder (i-1,j-1))
+        matrix (nrows m +2) (ncols m +2) (\(i,j) -> builder (i-1,j-1))
     else
         matrix (nrows m) (ncols m) builder
     where
         builder (i,j) = 
-            if (get (i,j) == 0) then
-                if (hasToBorn (i,j)) then
+            if get (i,j) == 0 then
+                if hasToBorn (i,j) then
                     1
                 else
                     0
             else
-                if (hasToDie (i,j)) then
+                if hasToDie (i,j) then
                     0
                 else
                     1
@@ -38,7 +38,7 @@ iterate m =
             Vector.sum (getRow (nrows m) m) > 0 ||
             Vector.sum (getCol 1 m) > 0 ||
             Vector.sum (getRow 1 m) > 0
-        get (i,j) = Maybe.maybe 0 (\x->x) (safeGet i j m)
+        get (i,j) = Maybe.fromMaybe 0 (safeGet i j m)
         hasToBorn (i,j) = sumNeighbors (i,j) == 3
         hasToDie (i,j) = sumNeighbors (i,j) /= 2 && sumNeighbors (i,j) /= 3
         sumNeighbors (i,j) = 
--- app/Reader.hs
+++ app/Reader.hs
@@ -6,8 +6,8 @@ import qualified Data.Matrix as Matrix
 readVacaFile :: IO (Matrix.Matrix Int)
 readVacaFile = do
     file <- readFile "gosper.txt"
-    let alto = read ((lines file) !! 0) :: Int
-    let ancho = read ((lines file) !! 1) :: Int
+    let alto = read (lines file !! 0) :: Int
+    let ancho = read (lines file !! 1) :: Int
     let m = drop 2 (lines file)
     let matrix = buildMatrix alto ancho m
     return matrix
@@ -15,4 +15,4 @@ readVacaFile = do
 buildMatrix :: Int -> Int -> [String] -> Matrix.Matrix Int
 buildMatrix alto ancho m = Matrix.matrix alto ancho builder 
     where
-        builder (i,j) = if ((m !! (i-1)) !! (j-1) == 'X') then 1 else 0
+        builder (i,j) = if (m !! (i-1)) !! (j-1) == 'X' then 1 else 0
